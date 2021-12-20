5dff3a3a2c3022afae0f58668f287c1f8758c3d6
Theerapat patbang <644230041@webmail.npru.ac.th.com>
2021-12-20 16:21:05 +0700

    BasicJavaCode

---
+++ _01_MyfirstProgram.java
@@ -0,0 +1,12 @@
+package week01;
+
+class _01_MyfirstProgram {
+    public static void main(String [] args){
+        System.out.println("Welcome to Java Program");
+        System.out.println("====================");
+        System.out.println("My name is Suchkhri Oakkaraserani");
+        System.out.println("Age 18");
+        System.out.println("Unibersity : NPRU");
+
+    }
+}
